1a2598553f95c85406ea069d7010acf71a8cfbaf
Timofeev Yurii <georgius@ural.ru>
2020-08-27 11:03:02 +0500

    better formatting

--- ssr/store/index.js
+++ ssr/store/index.js
@@ -5,9 +5,10 @@ import getters from "./getters";
 import mutations from "./mutations";
 import actions from "./actions";
 
-export default () => new Vuex.Store({
-  state: () => state,
-  getters,
-  mutations,
-  actions
-});
+export default () =>
+  new Vuex.Store({
+    state: () => state,
+    getters,
+    mutations,
+    actions
+  });
